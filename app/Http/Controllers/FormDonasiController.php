2efc27834a02fd74bfd1b5897ae9dee6251dd443
abil0321 <salsabilmuhammad45@gmail.com>
2025-07-04 17:31:51 +0700

    fix midtrans

--- app/Http/Controllers/FormDonasiController.php
+++ app/Http/Controllers/FormDonasiController.php
@@ -38,9 +38,11 @@ class FormDonasiController extends Controller
         ]);
 
         // Set your Merchant Server Key
-        \Midtrans\Config::$serverKey = config('midtrans.server_key');
+        // \Midtrans\Config::$serverKey = config('midtrans.server_key');
+        \Midtrans\Config::$serverKey = 'SB-Mid-server-qGin1e_8LpPnWFLw_UPhQHh-';
         // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
-        \Midtrans\Config::$isProduction = config('midtrans.is_production');
+        \Midtrans\Config::$isProduction = false;
+        // \Midtrans\Config::$isProduction = config('midtrans.is_production');
         // Set sanitization on (default)
         \Midtrans\Config::$isSanitized = true;
         // Set 3DS transaction for credit card to true
@@ -73,7 +75,8 @@ class FormDonasiController extends Controller
     }
 
     public function callback(Request $request){
-        $serverKey = config('midtrans.server_key');
+        $serverKey = 'SB-Mid-server-qGin1e_8LpPnWFLw_UPhQHh-';
+        // $serverKey = config('midtrans.server_key');
         $hashed = hash("sha512", $request->order_id . $request->status_code . $request->gross_amount . $serverKey);
         if($hashed == $request->signature_key){
             if($request->transaction_status == 'capture' or $request->transaction_status == 'settlement'){
