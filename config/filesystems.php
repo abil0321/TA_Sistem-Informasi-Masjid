f564075638c32799510390938fe781a52a613e99
abil0321 <salsabilmuhammad45@gmail.com>
2025-07-04 17:53:46 +0700

    fix upload file

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,8 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => 'https://masjid-muqorrobin.hostingphp.cloudapp.web.id/storage',
+            // 'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
